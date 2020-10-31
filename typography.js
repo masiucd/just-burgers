b50d4a2d011d3438937205ea9c21df0dfb78c854
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2020-10-31 11:05:46 +0100

    read me

--- typography.js
+++
@@ -1,8 +0,0 @@
-import Typography from "typography"
-import grandViewTheme from "typography-theme-grand-view"
-
-const typography = new Typography(grandViewTheme)
-
-// Export helper functions
-export const { scale, rhythm, options } = typography
-export default typography
