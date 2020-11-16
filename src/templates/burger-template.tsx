b447ae38aa932d1a2452b3d3fd71de10e4579428
Masiu <ciszekmarcell@gmail.com>
2020-11-16 19:12:58 +0100

    single component for different templates

--- src/templates/burger-template.tsx
+++ src/templates/burger-template.tsx
@@ -2,19 +2,8 @@ import { Layout } from "@components/layout"
 import { Seo } from "@components/Seo"
 import { graphql, PageProps } from "gatsby"
 import React from "react"
-import GatsbyImage from "gatsby-image"
-import { useToggle } from "@hooks/useToggle"
-import {
-  AnimatedInfo,
-  ItemBody,
-  ItemWrapper,
-  ItemTitle,
-  ImageWrapper,
-  Ingredients,
-  NamePrice,
-} from "@styled/item-styles"
-import { AnimatePresence } from "framer-motion"
-import { infoVariants, ingredientsVariants } from "@styled/variants"
+
+import Item from "@components/item-template/Item"
 interface BurgerDataProps {
   burger: Burger
 }
@@ -26,73 +15,11 @@ const BurgerTemplate: React.FC<PageProps<
   BurgerDataProps,
   PageContextProps
 >> = ({ data: { burger }, pageContext }) => {
-  const { state: on, toggle } = useToggle()
-
   return (
     <>
       <Seo title="burger" />
       <Layout>
-        <ItemWrapper>
-          <ImageWrapper>
-            <GatsbyImage
-              fluid={burger.image?.fluid}
-              alt={`burger-${burger.slug}`}
-            />
-          </ImageWrapper>
-          <AnimatePresence>
-            <ItemBody
-              key="burger-body"
-              initial={{ opacity: 0 }}
-              animate={
-                on
-                  ? { opacity: 1, transition: { duration: 0.3 } }
-                  : { opacity: 0 }
-              }
-              exit={{ opacity: 0 }}
-            >
-              <div className="column-flex-column">
-                <NamePrice>
-                  <p> {burger.name} </p>
-                  <small>{burger.price}$</small>
-                </NamePrice>
-                <AnimatedInfo
-                  vegetarian={burger.vegetarian}
-                  initial="closed"
-                  animate={on ? "open" : "closed"}
-                  variants={infoVariants}
-                  transition={{ duration: 0.6, damping: 8 }}
-                >
-                  <p> {burger.desc?.desc} </p>
-                  <p>
-                    {burger.vegetarian
-                      ? "for the green lovers 🥗 "
-                      : "for the meet lovers 🥩"}{" "}
-                  </p>
-                </AnimatedInfo>
-              </div>
-              <div className="column-flex-row">
-                <Ingredients
-                  initial="closed"
-                  animate={on ? "open" : "closed"}
-                  variants={ingredientsVariants}
-                  transition={{ duration: 0.6, damping: 8 }}
-                >
-                  {burger.ingredients?.ingredients.map(ingredient => (
-                    <li key={ingredient}>{ingredient}</li>
-                  ))}
-                </Ingredients>
-              </div>
-            </ItemBody>
-          </AnimatePresence>
-          <ItemTitle
-            initial={{ opacity: 0, x: "1000%" }}
-            animate={{ opacity: 1, x: "-50%" }}
-            transition={{ damping: 6, duration: 0.25 }}
-            onClick={toggle}
-          >
-            <h4>{burger.name} ⬇ </h4>
-          </ItemTitle>
-        </ItemWrapper>
+        <Item item={burger} />
       </Layout>
     </>
   )
--- src/templates/side-template.tsx
+++ src/templates/side-template.tsx
@@ -2,19 +2,6 @@ import React from "react"
 import { Seo } from "@components/Seo"
 import { Layout } from "@components/layout"
 import { graphql, PageProps } from "gatsby"
-import {
-  AnimatedInfo,
-  ImageWrapper,
-  Ingredients,
-  ItemBody,
-  ItemTitle,
-  ItemWrapper,
-  NamePrice,
-} from "@styled/item-styles"
-import GatsbyImage from "gatsby-image"
-import { AnimatePresence } from "framer-motion"
-import { useToggle } from "@hooks/useToggle"
-import { infoVariants, ingredientsVariants } from "@styled/variants"
 import Item from "@components/item-template/Item"
 
 interface SideDataProps {
@@ -24,72 +11,11 @@ interface SideDataProps {
 const SideTemplate: React.FC<PageProps<SideDataProps, {}>> = ({
   data: { side },
 }) => {
-  const { state: on, toggle } = useToggle()
-
-  const { value: desc } = side.desc_.content[0].content[0]
   return (
     <>
       <Seo title={`side-${side.slug}`} />
       <Layout>
         <Item item={side} />
-        {/* <ItemWrapper>
-          <ImageWrapper>
-            <GatsbyImage fluid={side.image?.fluid} alt={`side-${side.slug}`} />
-          </ImageWrapper>
-          <AnimatePresence>
-            <ItemBody
-              key="burger-body"
-              initial={{ opacity: 0 }}
-              animate={
-                on
-                  ? { opacity: 1, transition: { duration: 0.3 } }
-                  : { opacity: 0 }
-              }
-              exit={{ opacity: 0 }}
-            >
-              <div className="column-flex-column">
-                <NamePrice>
-                  <p> {side.title} </p>
-                  <small>{side.price}$</small>
-                </NamePrice>
-                <AnimatedInfo
-                  vegetarian={side.vegetarian}
-                  initial="closed"
-                  animate={on ? "open" : "closed"}
-                  variants={infoVariants}
-                  transition={{ duration: 0.6, damping: 8 }}
-                >
-                  <p> {desc} </p>
-                  <p>
-                    {side.vegetarian
-                      ? "for the green lovers 🥗 "
-                      : "for the meet lovers 🥩"}{" "}
-                  </p>
-                </AnimatedInfo>
-              </div>
-              <div className="column-flex-row">
-                <Ingredients
-                  initial="closed"
-                  animate={on ? "open" : "closed"}
-                  variants={ingredientsVariants}
-                  transition={{ duration: 0.6, damping: 8 }}
-                >
-                  {side.ingredients?.ingredients.map(ingredient => (
-                    <li key={ingredient}>{ingredient}</li>
-                  ))}
-                </Ingredients>
-              </div>
-            </ItemBody>
-          </AnimatePresence>
-          <ItemTitle
-            initial={{ opacity: 0, x: "1000%" }}
-            animate={{ opacity: 1, x: "-50%" }}
-            transition={{ damping: 6, duration: 0.25 }}
-            onClick={toggle}
-          >
-            <h4>{side.title} ⬇ </h4>
-          </ItemTitle>
-        </ItemWrapper> */}
       </Layout>
     </>
   )
