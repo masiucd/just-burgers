7c8b685749c23342f988fc9e5e6daaae5ddf7b71
Masiu <ciszekmarcell@gmail.com>
2020-11-20 20:08:13 +0100

    path resolve

--- src/styled/index.ts
+++ src/styled/index.ts
@@ -11,7 +11,7 @@ import { Strong } from "./HtmlElements"
 import { below, above } from "./media-query"
 import { TwoColumnGrid, Column } from "./Wrappers"
 import { GlobalStyles } from "./GlobalStyles"
-import Typography from "./typography"
+import Typography from "./Typography"
 import { mainTheme } from "./theme"
 import { Button } from "./Buttons"
 export {
